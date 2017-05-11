0cb5baf0defafc3a4b698afb1a872d197c6999dc
scmsqhn <haining.qin@changhong.com>
2017-05-11 11:37:49 +0800

    17-05-11

---
+++ yydb-master/pages/home/tpl/goodsList.js
@@ -0,0 +1,19 @@
+var app = getApp()
+Page( {
+  onReady: function() {
+
+    console.log( 'onReady' );
+  },
+  onLoad: function() {
+    console.log( 'onLoad' );
+  },
+  onShow: function() {
+    console.log( 'onShow' );
+
+  },
+  onToTop: function( e ) {
+    console.log( e.detail.scrollTop )
+  },
+  
+
+})
